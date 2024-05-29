95bfdd70b3ba8eeed97aaf38ecf511503f2be3c0
Streltsov Ivan <principdereva@gmail.com>
2024-05-29 11:52:14 +0200

    added AdSchema

---
+++ app/models/admodel.js
@@ -0,0 +1,34 @@
+const { Schema, model, Types } = require('mongoose');
+
+const AdSchema = new Schema({
+    shortText: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    images: {
+        type: String,
+        required: false,
+    },
+    userId: {
+        type: Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    tags: {
+        type: [String],
+        required: false,
+    },
+    isDeleted: {
+        type: Boolean,
+        required: true,
+        default: false,
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = model('Ad', AdSchema);
