c69fc52b325004900e8a97634380c76ad5ac30d2
Streltsov Ivan <principdereva@gmail.com>
2024-05-30 16:20:35 +0200

    added verification

--- app/index.js
+++ app/index.js
@@ -1,14 +1,32 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const mongoose = require('mongoose');
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
+const UserModel = require('./models/usermodel')
 
 const advertisementsRoute = require('./routes/advertisements-route')
 const signinRoute = require('./routes/signin-route')
-const signupRoute = require('./routes/signup-route');
+const signupRoute = require('./routes/signup-route')
+
+const app = express();
+const server = http.createServer(app);
 
 app.use(express.json())
 app.use('/static', express.static(path.join(__dirname, 'static')))
+
+const options = {
+  usernameField: 'username',
+  passwordField: 'password',
+};
+
+app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: 'SECRET', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 
 const PORT = process.env.PORT || 3000
 const UrlDB = process.env.URL_DB
@@ -17,11 +35,51 @@ app.use('/api/advertisements', advertisementsRoute)
 app.use('/api/signin', signinRoute)
 app.use('/api/signup', signupRoute)
 
+app.get('/api/check-auth', isAuthenticated, (req, res) => {
+  res.status(200).json({ message: 'User is authenticated' })
+});
+
+function isAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.status(401).json({ message: 'Unauthorized' })
+}
 
 async function start(PORT, urlDb) {
   try {
-    await mongoose.connect(urlDb, { dbName: 'ads' });
-    app.listen(PORT, async () => {
+    await mongoose.connect(urlDb, { dbName: 'ads' })
+
+    const verify = async (username, password, done) => {
+      const user = await UserModel.findOne({ username: username }).select('-__v')
+      if (!user) {
+          return done(null, false, {message: 'Incorrect email'});
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      
+      if (!isMatch) {
+        return done(null, false, { message: 'Неверный пароль' });
+      }
+
+      return done(null, user);
+    };
+
+    passport.serializeUser((user, cb) => {
+        cb(null, user.id);
+    });
+
+    passport.deserializeUser(async (id, cb) => {
+        const user = await UserModel.findById(id).select('-__v');
+        if (!user) {
+            return cb(new Error('User not found'));
+        }
+        cb(null, user);
+    });
+
+    passport.use(new LocalStrategy(options, verify));
+
+    server.listen(PORT, async () => {
       console.log('Server is running on port', PORT)
     })
   } catch (error) {
--- app/routes/signin-route.js
+++ app/routes/signin-route.js
@@ -1,42 +1,7 @@
 const express = require('express')
-const UserModel = require('../models/usermodel')
-const bcrypt = require('bcryptjs')
+const passport = require('passport')
 const router = express.Router()
 
-router.get('/', async (req, res) => {
-    const {email, password, name, contactPhone} = req.body
-
-    const hashedPassword = bcrypt.hash(password, 10)
-
-    try {
-        const newUser = await UserModel.create({
-            email,
-            name,
-            contactPhone,
-            passwordHash: hashedPassword
-        });
-
-        return res.send({
-                "data": {
-                  "id": newUser._id,
-                  "email": newUser.email,
-                  "name": newUser.name,
-                  "contactPhone": newUser.contactPhone
-                },
-                "status": "ok"
-        })
-
-    } catch (error) {
-        return res.status(500).send({
-            "error": "email занят",
-            "status": "error"
-        });
-    }
-
-
-
-
-
-})
+router.post('/', passport.authenticate('local'))
 
 module.exports = router;
