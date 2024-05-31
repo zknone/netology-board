9b1505190b272a7fa7534b109a6758d78942e4bf
Streltsov Ivan <principdereva@gmail.com>
2024-05-31 17:47:25 +0200

    feat: user images folders

--- app/middleware/upload.js
+++ app/middleware/upload.js
@@ -1,8 +1,27 @@
 const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    const userId = req.user._id;
+    const uploadPath = path.join('uploads', userId.toString());
+
+    // eslint-disable-next-line node/prefer-promises/fs
+    fs.access(uploadPath, (error) => {
+      if (error) {
+        // eslint-disable-next-line node/prefer-promises/fs
+        fs.mkdir(uploadPath, { recursive: true }, (err) => {
+          if (err) {
+            console.error('Failed to create directory', err);
+            return cb(err);
+          }
+          cb(null, uploadPath);
+        });
+      } else {
+        cb(null, uploadPath);
+      }
+    });
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
